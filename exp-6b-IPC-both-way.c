d9d568b3748c4b5f2a4b033d539400b42acf9829
Aditya Pratap Singh Rajput <apsrcreatix@gmail.com>
2017-09-20 15:12:45 +0530

    Added comment for better understanding of code
    Commenting is healthy.

--- exp-6b-IPC-both-way.c
+++ exp-6b-IPC-both-way.c
@@ -11,20 +11,28 @@ parent and child usnig pipes.
 
 int main(){
 	int fd1[2],fd2[2],nbytes1,nbytes2;
+	/*pid_t data type is a signed integer type which is capable of representing a process ID. 
+	*header files needed : 
+                *unistd.h
+                *sys/types.h
+	*/
 	pid_t childpid;
-	char string1[] = "How Are You ?\n";//message 1
-	char string2[] = "I am fine\n";//message 2
+	char string1[] = "How Are You ?\n";
+	char string2[] = "I am fine\n";
+	//initialising two readbuffers
 	char readbuffer[80],readbuffer1[80];
-
+//creating pipes for pipelining 
 pipe(fd1);
 pipe(fd2);
+//creating a process
 childpid = fork();
 	if (childpid==-1)
 	 {
 	printf("Erro\n");
 	}
 	if (childpid==0)
-	 {		
+	 {	
+	 //closing writing section of fd2 then accessing reading section by read
 	 	close(fd2[1]);
 	 	nbytes2=read(fd2[0],readbuffer1,sizeof(readbuffer1));
 	 	printf("Child : %s\n",readbuffer1);
@@ -33,8 +41,10 @@ childpid = fork();
 	 	write(fd1[1],string2,strlen(string2)+1);
 
 	 }else{
+	 //closing reading section of fd2 then accessing writing section by read
 	 	close(fd2[0]);
 	 	write(fd2[1],string1,strlen(string1)+1);
+	 //closing writing section of fd1 then accessing reading section by read
 	 	close(fd1[1]);
 	 	nbytes1=read(fd1[0],readbuffer,sizeof(readbuffer));
 	 	printf("Parent : %s\n",readbuffer );
